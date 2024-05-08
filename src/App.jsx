44a2666a950c76cd28e4477005ef058880758085
Muskan Jaiswal <muskanj9161@gmail.com>
2024-05-08 20:41:24 +0530

    app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import AllRoutes from './AllRoutes'
+import Home from './Pages/Home/Home'
+import Otp from './Pages/Otp/Otp'
+import Login from './Pages/Login/Login'
+import Signup from './Pages/Signup/Signup'
 
 const App = () => {
   return (
     <div className='App'>
         <Router>
             {/* Navbar */}
+            <Home/>
+            <Otp/>
+            <Login/>
+            <Signup/>
             <AllRoutes/>
         </Router>
     </div>
