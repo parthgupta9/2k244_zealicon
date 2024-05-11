1b66270b8e81671cc6915d710ae65cc3821d7251
Piyush Yadav <piyushya012@gmail.com>
2024-05-11 13:12:50 +0530

    added ham-menu

--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import logo from "../../assets/Frame.png";
 import bg from "../../assets/BG 1.png";
 import bg_phone from "../../assets/BG_phone.png";
@@ -8,12 +8,17 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchZealId } from "../../actions/zeal";
 import { LOGIN_STARTED, LOGOUT } from "../../actions/actionType/actionType";
 import { toast } from "react-toastify";
+import Hamburger from "hamburger-react";
+import { gsap } from "gsap";
 
 const Navbar = ({ setIsModalOpen }) => {
   const [windowSize, setWindowSize] = useState({
     width: window.innerWidth,
     height: window.innerHeight,
   });
+
+  const [isOpen, setOpen] = useState(false);
+
   const updateWindowSize = () => {
     setWindowSize({
       width: window.innerWidth,
@@ -21,14 +26,51 @@ const Navbar = ({ setIsModalOpen }) => {
     });
   };
 
+  React.useEffect(() => {
+    window.addEventListener("resize", updateWindowSize, false);
+  }, []);
+
   const dispatch = useDispatch();
   const { zealId, isAuthenticated } = useSelector((state) => state.allReducers);
+
   useEffect(() => {
     const fetchId = async () => {
       dispatch(fetchZealId());
     };
     fetchId();
   }, []);
+
+  function NavMenu() {
+    return (
+      <>
+        <nav className={styles.mobileNav}>
+          <ul className={styles.mobileNavLinksCont}>
+            <li>About</li>
+            <li>Events</li>
+            <li>Team</li>
+            <li>Download App</li>
+          </ul>
+          {zealId && isAuthenticated ? (
+            <Button
+              type={"small"}
+              text={"LOGOUT"}
+              action={() => {
+                toast.success("Logout Successfully!");
+                return dispatch({ type: LOGOUT });
+              }}
+            />
+          ) : (
+            <Button
+              type={"small"}
+              text={"LOGIN"}
+              action={() => setIsModalOpen(true)}
+            />
+          )}
+        </nav>
+      </>
+    );
+  }
+
   return (
     <>
       <div className={`${styles.container}`}>
@@ -37,30 +79,42 @@ const Navbar = ({ setIsModalOpen }) => {
           src={windowSize.width <= 900 ? bg_phone : bg}
           alt="Your Image"
         />
+
         <div className={`${styles.list}`}>
           <img src={logo}></img>
-          <ul className={`${styles.ul}`}>
-            <li>About</li>
-            <li>Events</li>
-            <li>Team</li>
-            <li>Download App</li>
-            {zealId && isAuthenticated ? (
-              <Button
-                type={"small"}
-                text={"LOGOUT"}
-                action={() => {
-                  toast.success("Logout Successfully!");
-                  return dispatch({ type: LOGOUT });
-                }}
-              />
-            ) : (
-              <Button
-                type={"small"}
-                text={"LOGIN"}
-                action={() => setIsModalOpen(true)}
-              />
-            )}
-          </ul>
+          {windowSize.width > 900 ? (
+            <ul className={`${styles.ul}`}>
+              <li>About</li>
+              <li>Events</li>
+              <li>Team</li>
+              <li style={{marginRight: "3rem"}}>Download App</li>
+              {zealId && isAuthenticated ? (
+                <Button
+                  type={"small"}
+                  text={"LOGOUT"}
+                  action={() => {
+                    toast.success("Logout Successfully!");
+                    return dispatch({ type: LOGOUT });
+                  }}
+                  size={3}
+                />
+              ) : (
+                <Button
+                  type={"small"}
+                  text={"LOGIN"}
+                  action={() => setIsModalOpen(true)}
+                  size={3}
+                />
+              )}
+            </ul>
+          ) : (
+            <>
+              <div className={styles.hamIcon}>
+                <Hamburger rounded toggled={isOpen} toggle={setOpen} />
+              </div>
+              {isOpen && <NavMenu />}
+            </>
+          )}
         </div>
       </div>
 
@@ -68,7 +122,7 @@ const Navbar = ({ setIsModalOpen }) => {
         <p className={`${styles.date}`}>20th May-22th May</p>
         <div className={`${styles.btnBox}`}>
           <Button type={"small"} text={"Download App"} action={() => {}} />
-          {zealId? (
+          {zealId ? (
             <Button
               type={"small"}
               text={"Your Zeal ID"}
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,14 +1,15 @@
 import styles from "./Button.module.css";
 
-export default function Button({ type, text, action }) {
+export default function Button({ type, text, action, size=3.5 }) {
   return (
     <div role="button" onClick={action} className={styles.buttonCont}>
       <img
         src={
-          type === "small" 
+          type === "small"
             ? "./images/smallButtonBg.svg"
             : "./images/largeButtonBg.svg"
         }
+        style={{ height: `${size}rem` }}
         aria-hidden
       />
       <p>{text}</p>
