f30ea90c9ce9442e55482e73089a5a8737fb2895
parthgupta9 <parth615gupta@gmail.com>
2024-05-14 01:24:42 +0530

    images

--- src/Pages/Team/data.jsx
+++ src/Pages/Team/data.jsx
@@ -1,13 +1,13 @@
-import anant from "../../assets/images/Team/anant.png";
+
 import anadee from "../../assets/images/Team/anadee.png";
 import anuj from "../../assets/images/Team/anuj_agarwal.png";
 import arnika from "../../assets/images/Team/arnika_sharma.png";
-import ayush from "../../assets/images/Team/ayush_kumar.png";
+
 import harshit from "../../assets/images/Team/harshit_keshri.png";
 import jayati from "../../assets/images/Team/jayati_dixit.png";
 import kavya from "../../assets/images/Team/kavya_daga.png";
 import krapi from "../../assets/images/Team/krapi_rastogi.png";
-import naman from "../../assets/images/Team/naman_shankydar.png";
+
 import ojas from "../../assets/images/Team/ojuswi_rastogi.png";
 import paarth from "../../assets/images/Team/parth_agarwal.png";
 import prince from "../../assets/images/Team/prince_gupta.png";
@@ -19,132 +19,156 @@ import utkarsh from "../../assets/images/Team/utkarsh_prakash.png";
 import akshit from "../../assets/images/Team/akshit.png";
 import anikesh from "../../assets/images/Team/anikesh.png";
 import anukriti from "../../assets/images/Team/anukriti.png";
-import ashish from "../../assets/images/Team/ashish.png";
+
 import chirag from "../../assets/images/Team/chirag.png";
 import djseth from "../../assets/images/Team/djseth.png";
-import unknwown from "../../assets/images/Team/unknown.png";
+
 import kushal from "../../assets/images/Team/kushal.png";
 import manthan from "../../assets/images/Team/manthan.png";
 import mukul from "../../assets/images/Team/mukul2.jpg";
 import niharika from "../../assets/images/Team/nihsarika.png";
 import pranjal from "../../assets/images/Team/pranjal.png";
-import samridh from "../../assets/images/Team/samridh.png";
+
 import shashwat from "../../assets/images/Team/shashwat.png";
 import shubhanshu from "../../assets/images/Team/shubhanshu.png";
 import utsav from "../../assets/images/Team/utsav.png";
-import yashvant from "../../assets/images/Team/yashwnt.png";
-import saransh from "../../assets/images/Team/Saransh.jpeg";
+
+
+
+import anant from "../../assets/POST/4.png"
+import abhimanya from "../../assets/POST/5.png"
+import Fardin from "../../assets/POST/6.png"
+import yashasvi from "../../assets/POST/7.png"
+import ujjjwal from "../../assets/POST/8.png"
+import sourabh from "../../assets/POST/9.png"
+import Tanay from "../../assets/POST/10.png"
+import Rishabh from "../../assets/POST/11.png"
+import Priyanshu from "../../assets/POST/12.png"
+import krishna from "../../assets/POST/13.png"
+import gokaran from "../../assets/POST/14.png"
+import ayush from "../../assets/POST/15.png"
+import aditya from "../../assets/POST/16.png"
+import touquir from "../../assets/POST/17.png"
+import garvit from "../../assets/POST/18.png"
+import prakhar from "../../assets/POST/20.png"
+import mayank from "../../assets/POST/21.png"
+import manas from "../../assets/POST/22.png"
+import Harsh from "../../assets/POST/23.png"
+import sachin from "../../assets/POST/24.png"
+import abhimanyu from "../../assets/POST/25.png"
+import Ayush from "../../assets/POST/26.png"
+
 const teamData = [
   {
     id: 11,
-    name: "Apoorv Seth",
+    name: "Manas Goel",
     designation: "Fest Secretory",
-    photo: djseth,
+    photo: manas,
   },
   {
     id: 12,
-    name: "Utsav Arora",
+    name: "Anant Mishra",
     designation: "Co-Fest Secretory",
-    photo: utsav,
+    photo: anant,
   },
   {
     id: 13,
-    name: "Mukul Yadav",
+    name: "Tanay Pratap Singh",
     designation: "Resource Head",
-    photo: mukul,
+    photo: Tanay,
   },
   {
     id: 15,
-    name: "Saransh Pataria",
+    name: "Sachin Gautam",
     designation: "Artist & Production Head",
-    photo: saransh,
+    photo: sachin,
   },
   {
     id: 14,
-    name: "Kushal Gautam",
+    name: "Krishna Prakhar",
     designation: "Technical Head",
-    photo: kushal,
+    photo: krishna,
   },
   {
     id: 2,
-    name: "Naman Shankydar",
+    name: "Prakhar Sharma",
     designation: "Technical Head",
-    photo: naman,
+    photo: prakhar,
   },
 
   {
     id: 21,
-    name: "Akshit Srivastava",
+    name: "Fardin Khan",
     designation: "Security Head",
-    photo: akshit,
+    photo: Fardin,
   },
   {
     id: 1,
-    name: "Ayush Kumar",
-    designation: "Co-Security Head",
-    photo: ayush,
+    name: "Ayush Rathore",
+    designation: "Security Head",
+    photo: Ayush,
   },
 
   {
     id: 18,
-    name: "Shashwat Melhotra",
+    name: "Gokaran",
     designation: "Management Head",
-    photo: shashwat,
+    photo: gokaran,
   },
   {
     id: 19,
-    name: "Yashvant Singh",
-    designation: "Co-Management Head",
-    photo: yashvant,
+    name: "Mohd Tauquir Ahmad",
+    designation: "Management Head",
+    photo: touquir,
   },
 
   {
     id: 3,
-    name: "Utkarsh Prakash",
+    name: "Ujjwal Saxena",
     designation: "Creative Head",
-    photo: utkarsh,
+    photo: ujjjwal,
   },
   {
     id: 24,
-    name: "Anukriti Attreya",
+    name: "Priyanshu Bharti",
     designation: "Culural Head",
-    photo: anukriti,
+    photo: Priyanshu,
   },
   {
     id: 25,
-    name: "Niharika Malviya",
+    name: "Aditya Singh",
     designation: "Marketing Head",
-    photo: niharika,
+    photo: aditya,
   },
   {
     id: 26,
-    name: "Ashish Pandey",
+    name: "Harsh Jain",
     designation: "Media Head",
-    photo: ashish,
+    photo: Harsh,
   },
   {
     id: 76,
-    name: "Shashikant Mishra",
+    name: "Abhimanyu Singh Naruka",
     designation: "Finance Head",
-    photo: unknwown,
+    photo: abhimanyu,
   },
   {
     id: 27,
-    name: "Shubhanshu Shekhar",
+    name: "Sourabh Singhr",
     designation: "Merchandise Head",
-    photo: shubhanshu,
+    photo: sourabh,
   },
   {
     id: 28,
-    name: "Anikesh Singh",
+    name: "Ayush pandey",
     designation: "Merchandise Head",
-    photo: anikesh,
+    photo: ayush,
   },
   {
     id: 5,
-    name: "Kavya Daga",
+    name: "Mayank Majila",
     designation: "Sponsorship Head",
-    photo: kavya,
+    photo: mayank,
   },
   {
     id: 87,
@@ -155,22 +179,22 @@ const teamData = [
 
   {
     id: 51,
-    name: "Chirag Gupta",
+    name: "Garvit Kumar",
     designation: "Decoration Head",
-    photo: chirag,
+    photo: garvit,
   },
 
   {
     id: 52,
-    name: "Manthan Srivastava",
+    name: "Abhimanyu Singh",
     designation: "Student welfare ",
-    photo: manthan,
+    photo: abhimanya,
   },
   {
     id: 27,
-    name: "Pranjal Shakya",
+    name: "Yashasvi Tyagi",
     designation: "Social Media Head",
-    photo: pranjal,
+    photo: yashasvi,
   },
   // {
   //   id: 27,
